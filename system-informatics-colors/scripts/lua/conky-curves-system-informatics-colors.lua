06050ad7e72aabf823856f6e51cab921742e365f
Alejandro Alcalde <algui91@gmail.com>
2018-02-19 12:18:13 +0100

    Updated conky config

--- system-informatics-colors/scripts/lua/conky-curves-system-informatics-colors.lua
+++ system-informatics-colors/scripts/lua/conky-curves-system-informatics-colors.lua
@@ -1,10 +1,10 @@
 -- Copyright 2015 Antonio Malcolm
---   
--- This file, and all contents herein, is subject to the terms of the Mozilla Public License, v. 2.0. 
--- If a copy of the MPL was not distributed with this file, 
+--
+-- This file, and all contents herein, is subject to the terms of the Mozilla Public License, v. 2.0.
+-- If a copy of the MPL was not distributed with this file,
 -- you can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- conky-curves-system-informatics-colors.lua - Generates usage curves with percentage arcs, useful in describing the states of various system resources. 
+-- conky-curves-system-informatics-colors.lua - Generates usage curves with percentage arcs, useful in describing the states of various system resources.
 --
 -- v2015.07.27
 --
@@ -36,7 +36,7 @@ local function drawToConkyWindow(cairoContext)
     local alphaSteps = {backgroundAlphaLowerBound}
     local alphaBoundsQuotient = (backgroundAlphaUpperBound - backgroundAlphaLowerBound) / stepCount
 
-    for idx = 1, (stepCount - 2) 
+    for idx = 1, (stepCount - 2)
     do
       table.insert(alphaSteps, (alphaSteps[idx] + alphaBoundsQuotient))
     end
@@ -51,8 +51,8 @@ local function drawToConkyWindow(cairoContext)
   -- Provides a mechanism for ensuring a color hexidecmial is returned from the array of available color hexidecimals.
   -- In the case that a provided index is out of bounds of the array, or no index is provided,
   -- a random index within bounds will be generated, and the hexidecimal at that index will be returned.
-  -- 
-  -- idx - number, optional, the index at which the color hexidecimal is located, or, 
+  --
+  -- idx - number, optional, the index at which the color hexidecimal is located, or,
   --       if the index is out of bounds, or not provided, a randomly-selected hexidecimal is returned
   local function getColorHexidecmial(idx)
 
@@ -81,8 +81,8 @@ local function drawToConkyWindow(cairoContext)
 
       cpu = {
         percentage_divisor = 100,
-        position_x = 346, 
-        position_y = 245,
+        position_x = 346,
+        position_y = 105,
         radius = 22,
         weight = 18,
         angle_start = 0,
@@ -90,13 +90,13 @@ local function drawToConkyWindow(cairoContext)
         color = getColorHexidecmial(3),
         alpha = 0.95,
         background_color = 0xf8f2eb,
-        background_alpha = backgroundAlphaUpperBound  
+        background_alpha = backgroundAlphaUpperBound
       },
 
       memory = {
         percentage_divisor = 100,
-        position_x = 75, 
-        position_y = 400,
+        position_x = 75,
+        position_y = 300,
         radius = 66,
         weight = 18,
         angle_start = 360,
@@ -104,13 +104,13 @@ local function drawToConkyWindow(cairoContext)
         color = getColorHexidecmial(5),
         alpha = 0.95,
         background_color = 0xf8f2eb, --0x091219,
-        background_alpha = backgroundAlphaUpperBound  
+        background_alpha = backgroundAlphaUpperBound
       },
 
       battery = {
         percentage_divisor = 100,
-        position_x = 166, 
-        position_y = 500,
+        position_x = 166,
+        position_y = 400,
         radius = 44,
         weight = 18,
         angle_start = 360,
@@ -118,13 +118,13 @@ local function drawToConkyWindow(cairoContext)
         color = getColorHexidecmial(4),
         alpha = 0.95,
         background_color = 0xf8f2eb,
-        background_alpha = backgroundAlphaUpperBound  
+        background_alpha = backgroundAlphaUpperBound
       },
 
       volume_use = {
         percentage_divisor = 100,
-        position_x = 456, 
-        position_y = 520,
+        position_x = 346,
+        position_y = 400,
         radius = 66,
         weight = 18,
         angle_start = 0,
@@ -132,14 +132,14 @@ local function drawToConkyWindow(cairoContext)
         color = getColorHexidecmial(7),
         alpha = 0.95,
         background_color = 0xf8f2eb,
-        background_alpha = backgroundAlphaUpperBound  
+        background_alpha = backgroundAlphaUpperBound
       },
 
       network_wired = {
         -- assumes gigabit...
-        percentage_divisor = 12000,
-        position_x = 105, 
-        position_y = 635,
+        percentage_divisor = 6500,
+        position_x = 105,
+        position_y = 550,
         radius = 66,
         weight = 18,
         angle_start = 360,
@@ -147,14 +147,14 @@ local function drawToConkyWindow(cairoContext)
         color = getColorHexidecmial(1),
         alpha = 0.95,
         background_color = 0xf8f2eb,
-        background_alpha = backgroundAlphaUpperBound  
+        background_alpha = backgroundAlphaUpperBound
       },
 
       network_wireless = {
         -- assumes wireless-n...
-        percentage_divisor = 6000,
-        position_x = 405, 
-        position_y = 750,
+        percentage_divisor = 6500,
+        position_x = 405,
+        position_y = 600,
         radius = 66,
         weight = 18,
         angle_start = 0,
@@ -162,7 +162,7 @@ local function drawToConkyWindow(cairoContext)
         color = getColorHexidecmial(1),
         alpha = 0.95,
         background_color = 0xf8f2eb,
-        background_alpha = backgroundAlphaUpperBound  
+        background_alpha = backgroundAlphaUpperBound
       }
 
     }
@@ -171,8 +171,8 @@ local function drawToConkyWindow(cairoContext)
 
   end
 
-  
-  -- Converts a hexidecimal number and alpha number into decimal numbers with the appended alpha number digestable by Cairo method cairo_set_source_rgba(). 
+
+  -- Converts a hexidecimal number and alpha number into decimal numbers with the appended alpha number digestable by Cairo method cairo_set_source_rgba().
   --
   -- hexidecimal - number, required, the color hexidecimal to be converted to decimals
   -- alpha - number, the alpha transparency to be appended to the converted decimals
@@ -199,7 +199,7 @@ local function drawToConkyWindow(cairoContext)
     local arcLength = percentage * (angleEnd - angleStart)
     local isInverted = angleStart > angleEnd
 
-	  -- draw the curve...
+      -- draw the curve...
 
     if isInverted
     then
@@ -212,7 +212,7 @@ local function drawToConkyWindow(cairoContext)
     cairo_set_line_width(cairoContext, weight)
     cairo_stroke(cairoContext)
 
-	  -- draw the percentage arc...
+      -- draw the percentage arc...
 
     angleEnd = angleStart + arcLength
 
@@ -252,13 +252,13 @@ local function drawToConkyWindow(cairoContext)
 
   -- draw CPUs (or cores, or threads, whichever the case may be)
 
-    local cpuPercentageCommand=os.getenv('HOME')..'/.conky/system-informatics-colors/scripts/posix/cpus-system-informatics-colors.sh true'  
+    local cpuPercentageCommand=os.getenv('HOME')..'/.conky/system-informatics-colors/scripts/posix/cpus-system-informatics-colors.sh true'
     local cpuPercentagesString = nil
-  
+
     -- start by attempting to get the number of CPUs, cores, or threads from the system...
     local cpuPercentageResponseOk, cpuPercentageResponse = pcall(io.popen, cpuPercentageCommand)
-  
-  
+
+
     -- in case the user's system doesn't support Lua's io.popen()...
 
     if cpuPercentageResponseOk and cpuPercentageResponse
@@ -271,7 +271,7 @@ local function drawToConkyWindow(cairoContext)
 
     if cpuPercentagesString ~= nil
     then
-  
+
       cpuPercentages = {}
 
       for percentage in string.gmatch(cpuPercentagesString, "([^,]+)")
@@ -279,43 +279,43 @@ local function drawToConkyWindow(cairoContext)
         percentage = conky_parse(percentage) / 100
         table.insert(cpuPercentages, percentage)
       end
-  
+
       local cpuCount = table.getn(cpuPercentages)
 
       local alphaSteps = getBackgroundAlphaSteps(cpuCount)
-  
+
       -- Step backwards through the number of CPUs (or cores, or threads), so the first curve is always outermost...
-  
+
       for idx = cpuCount, 1 , -1
       do
-  
-        -- because we are stepping backwards through the CPU count, 
-        -- we need to obtain the inverse of the current index, 
+
+        -- because we are stepping backwards through the CPU count,
+        -- we need to obtain the inverse of the current index,
         -- to set (increase) the radius for each new curve,
         -- as well as ensure that data obtained from arrays are taken from the correct indices...
-     
+
         local idxInverse = (cpuCount - idx) + 1
-  
+
         local cpuCurveDescriptor = getCurveDescriptor('cpu')
         cpuCurveDescriptor['radius'] = cpuCurveDescriptor['radius'] * idxInverse
         cpuCurveDescriptor['color'] = getColorHexidecmial(idx)
         cpuCurveDescriptor['background_alpha'] = alphaSteps[idxInverse]
-  
+
         drawPercentageCurve(cpuCurveDescriptor, cpuPercentages[idx])
-  
+
       end
-  
+
     else
-  
+
      -- default to displaying the average overall CPU load, per Conky (LAST RESORT)...
- 
+
      local cpuCurveDescriptor = getCurveDescriptor('cpu')
      cpuCurveDescriptor['radius'] = cpuCurveDescriptor['radius'] * 3
- 
+
      drawPercentageCurveFromConkyValue(cpuCurveDescriptor, '${cpu cpu0}')
 
   end
- 
+
 
   -- draw RAM and swap...
 
@@ -344,7 +344,7 @@ local function drawToConkyWindow(cairoContext)
   drawPercentageCurveFromConkyValue(getCurveDescriptor('volume_use'), '${fs_used_perc /}')
   drawPercentageCurveFromConkyValue(homeCurveDescriptor, '${fs_used_perc /home}')
   drawPercentageCurveFromConkyValue(ssdCurveDescriptor, '${fs_used_perc /home/hkr/ssd2}')
-  
+
   -- draw battery...
 
   drawPercentageCurveFromConkyValue(getCurveDescriptor('battery'), '${battery_percent BAT0}')
@@ -376,7 +376,7 @@ local function drawToConkyWindow(cairoContext)
 end
 
 
--- Sets up the Cairo context, using the Conky display, 
+-- Sets up the Cairo context, using the Conky display,
 -- and passes it on to functions responsible for drawing data to the Conky window.
 -- Used by Conky, should be added to your Conky config (conkyrc) file, like so:
 -- lua_draw_hook_pre = 'conky_main'
@@ -388,31 +388,31 @@ function conky_main()
   end
 
   local cairoSurface = cairo_xlib_surface_create(conky_window.display, conky_window.drawable, conky_window.visual, conky_window.width, conky_window.height)
-  local cairoContext = cairo_create(cairoSurface)	
+  local cairoContext = cairo_create(cairoSurface)
 
-  if cairoContext == nil 
-  then 
-    return 
+  if cairoContext == nil
+  then
+    return
   end
 
   local updates = conky_parse('${updates}')
   updates = tonumber(updates)
 
-  -- in the case cpu updates are present in the curve descriptors, to prevent segfaults, 
+  -- in the case cpu updates are present in the curve descriptors, to prevent segfaults,
   -- ensure conky has had at least five updates, before drawing to the screen...
 
-  if updates > 5 
+  if updates > 5
   then
 --     local rh = rsvg_create_handle_from_file(os.getenv('HOME')..'/.conky/images/gentoo.svg')
 --     local rd = RsvgDimensionData:create()
 --     rsvg_handle_get_dimensions(rh, rd)
--- 
+--
 --     w, h, em, ex = rd:get()
--- 
+--
 --     print("width: "..w.." height: "..h.." em: "..em.." ex: "..ex)
 --     rsvg_handle_render_cairo(rh, cairoContext)
 --     rsvg_destroy_handle(rh)
--- 
+--
     drawToConkyWindow(cairoContext)
   end
 
